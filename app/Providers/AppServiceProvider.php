9fcd7e2880162f7189063ea344ff48a808fc7a3d
Karol <39806518+whichmann@users.noreply.github.com>
2025-07-26 19:12:33 +0200

    feat(): IDK

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,5 +1,3 @@
-<?php
-
 namespace App\Providers;
 
 use Illuminate\Routing\UrlGenerator;
@@ -7,22 +5,7 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @param UrlGenerator $url
-     * @return void
-     */
     public function boot(UrlGenerator $url)
     {
         if (env('APP_ENV') == 'production') {
