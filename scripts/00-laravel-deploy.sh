734304aecd437d052cd20ff57dd29c1d6c7f5f99
Karol <39806518+whichmann@users.noreply.github.com>
2025-07-26 19:09:05 +0200

    feat(): fix composer?

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 echo "Running composer"
-composer install --no-dev --working-dir=/var/www/html
+composer global require hirak/prestissimo
+composer install
 
 echo "Caching config..."
 php artisan config:cache
